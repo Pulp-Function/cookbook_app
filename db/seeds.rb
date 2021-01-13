862e79284d1df47ddba0a3d24390b79119727690
Peter Jang <peter.x.jang@gmail.com>
2021-01-12 19:03:25 -0600

    Add seeds file to make it easy to reset the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+recipe = Recipe.new({ title: "Raw eggs!!!", chef: "Jay Wengrow", ingredients: "One live chicken", directions: "Squeeze the chicken" })
+recipe.save
+
+recipe = Recipe.new(title: "Empty sandwich", chef: "Jay Wengrow", ingredients: "Two slices of bread", directions: "Put one slice of bread on top of the other")
+recipe.save
+
+recipe = Recipe.new(title: "Ice cubes", chef: "Peter Jang", ingredients: "Water", directions: "Place water in freezer.")
+recipe.save
